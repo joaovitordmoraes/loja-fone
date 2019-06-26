ad8596e0d36b075e6f5bb01085b81631f838de8f
João Vitor <joaovitordmoraes@gmail.com>
2019-06-26 02:43:16 +0000

    Desafio 02 - Aula 13

--- js/button-store.js
+++ js/button-store.js
@@ -1,1 +1,7 @@
 const $addcarrinho = window.document.querySelector('.-second');
+
+$addcarrinho.addEventListener('click', addCarrinho);
+
+function addCarrinho() {
+    console.log('Item adicionado no carrinho');
+}
